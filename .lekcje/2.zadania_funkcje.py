9f036f74b83c0c8e5c62c459fd8f2ed077204f92
NuniPol <wojkacz@gmail.com>
2026-03-07 16:28:43 +0100

    4. commit

--- .lekcje/2.zadania_funkcje.py
+++ .lekcje/2.zadania_funkcje.py
@@ -1,10 +1,10 @@
 #Zadanie 0.3
 #a
 def dodawanie_wektorów(u,v):
-    wyniki=[]
+    wynikiA=[]
     for i in range(len(u)):
-        wyniki.append(u[i]+v[i])
-    return wyniki
+        wynikiA.append(u[i]+v[i])
+    return wynikiA
 
 print(dodawanie_wektorów([3,2,4], [-4,7,-1]))
 
@@ -24,3 +24,27 @@ def czyanagramy2(a,b):
 
 czyanagramy3=lambda a,b:sorted(a) == sorted(b)
 
+#zadanie 0.3
+#b
+def mnożenie_wektorów(u,v):
+    wynikiB=[]
+    for i in range(len(u)):
+        wynikiB.append(u[i]*v[i])
+    return wynikiB
+#c
+def mnożenie_wektora(v,k):
+    wynikiC=[]
+    for i in range(len(v)):
+        wynikiC.append(v[i]*k)
+    return wynikiC
+
+#zadanie 2.2
+def jaki_trojkat(a,b,c):
+    lista=[a,b,c]
+    lista.sort()
+    if lista[0]**2+lista[1]**2==lista[2]**2:
+        print("prostokątny")
+    elif lista[0]**2+lista[1]**2>lista[2]**2:
+        print("ostrokątny")
+    else:
+        print("szerokokątny")
